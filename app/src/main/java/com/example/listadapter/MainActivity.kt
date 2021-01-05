043e66bc21e3c09519701c8a23c0de6e59d6124e
HienOc <hiendx.adnetplus@gmail.com>
2021-01-05 23:28:02 +0700

    hihuhu

--- app/src/main/java/com/example/listadapter/MainActivity.kt
+++ app/src/main/java/com/example/listadapter/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.listadapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val dummyRecyclerViewDiff by lazy {
+        DummyRecyclerViewDiff()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rcDummy.apply {
+            layoutManager = StaggeredGridLayoutManager(
+                1,
+                StaggeredGridLayoutManager.VERTICAL
+            )
+            adapter = dummyRecyclerViewDiff
+        }
+        val lisDummy= mutableListOf<Dummy>()
+        lisDummy.add(Dummy(1,"hhhh","LLLLL"))
+        lisDummy.add(Dummy(2,"hhhh","LLLLL"))
+        lisDummy.add(Dummy(3,"hhhh","LLLLL"))
+
+        dummyRecyclerViewDiff.submitList(lisDummy)
     }
+
 }
